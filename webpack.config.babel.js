1d1ac8c51c99ce6b050e7e9640a2241110c4fa1b
Eduardo S. Libardi <eslibardi@gmail.com>
2024-09-01 16:36:43 -0300

    build: generate commonJs (index.cjs) for common js
    In order to generate index.cjs I ended up upgrading webpack from 1 to 5 and babel from 6 to 7

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,19 +1,44 @@
-import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// Convert the `import.meta.url` to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const include = join(__dirname, 'src');
 
 export default {
-  entry: './src/index',
+  mode: 'development', // or 'production' based on your environment
+  entry: './src/index.js', // Explicitly specify the file extension for clarity
   output: {
     path: join(__dirname, 'dist'),
+    filename: '[name].js', // Specify the output filename
     libraryTarget: 'umd',
     library: 'spiritistBooks',
+    globalObject: 'this', // Ensure compatibility with both Node.js and browser environments
+    clean: false, // Automatically clean the output directory before each build
   },
   devtool: 'source-map',
   module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel', include },
-      { test: /\.json$/, loader: 'json', include },
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.json$/,
+        type: 'json', // Webpack 5 has built-in support for JSON
+        include,
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.json'], // Automatically resolve these extensions
   },
 };
