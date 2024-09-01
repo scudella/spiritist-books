ed7527044b7129121ceec916ddd1acff02268ff2
Eduardo S. Libardi <eslibardi@gmail.com>
2024-09-01 18:53:20 -0300

    feat: in order to make index.cjs commonjs to work, integrating the dependencies
    After creating index.cjs, it required creating spiritist-books.cjs and index.cjs from
    unique-random-array

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { spiritistBooks } from './spiritist-books.js';
-import uniqueRandomArray from 'unique-random-array';
+
 const getRandomItem = uniqueRandomArray(spiritistBooks);
 
 export default {
@@ -17,4 +17,53 @@ function random(number) {
     }
     return randomItems;
   }
+}
+
+function uniqueRandomArray(array) {
+  const random = consecutiveUniqueRandom(0, array.length - 1);
+  return () => array[random()];
+}
+
+function randomInteger(minimum, maximum) {
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+}
+
+function randomIntegerWithout(minimum, maximum, excludedValue) {
+  const number = randomInteger(minimum, maximum - 1);
+
+  return number >= excludedValue ? number + 1 : number;
+}
+
+function makeCallable(generator) {
+  const iterator = generator();
+
+  function random() {
+    return iterator.next().value;
+  }
+
+  random[Symbol.iterator] = function* () {
+    while (true) {
+      yield random();
+    }
+  };
+
+  return random;
+}
+
+function consecutiveUniqueRandom(minimum, maximum) {
+  return makeCallable(function* () {
+    if (minimum === maximum) {
+      while (true) {
+        yield minimum;
+      }
+    }
+
+    let previousValue = randomInteger(minimum, maximum);
+    yield previousValue;
+
+    while (true) {
+      previousValue = randomIntegerWithout(minimum, maximum, previousValue);
+      yield previousValue;
+    }
+  });
 }
